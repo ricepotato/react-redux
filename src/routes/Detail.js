9e0b92665fb4a7264d8d6acabbee5625a9310267
Sukjun Sagong <ricepotato40@gmail.com>
2020-05-28 20:36:26 +0900

    detao;

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,1 +1,44 @@
-export default () => "Detail";
+import React from "react";
+import { connect } from "react-redux";
+import { actionCreators } from "../store";
+import { useParams } from "react-router-dom";
+
+function Detail({ toDo, onBtnClick }) {
+  //console.log(props);
+  // const id = useParams();
+  // console.log(id);
+  return (
+    <>
+      <h1>{toDo?.text}</h1>
+      <h5>Created at: {toDo?.id}</h5>
+      <button onClick={onBtnClick}>delete</button>
+    </>
+  );
+  return;
+}
+
+function mapStateToProps(state, ownProps) {
+  const {
+    match: {
+      params: { id },
+    },
+  } = ownProps;
+  console.log(id, state);
+  //console.log(id);
+  return { toDo: state.find((toDo) => toDo.id === parseInt(id)) };
+}
+
+function mapDispatchToProps(dispatch, ownProps) {
+  return {
+    onBtnClick: () => {
+      const {
+        match: {
+          params: { id },
+        },
+      } = ownProps;
+      dispatch(actionCreators.deleteToDo(id));
+    },
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Detail);
